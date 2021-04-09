82267bfbc47e4e89d04cbef499626022b10a9cd8
LukeHarjulin <60152401+LukeHarjulin@users.noreply.github.com>
2021-04-09 17:26:16 +0100

    Added Key tests (need to fix Key)
    Key fails to actually construct for some reason.
    Needs fixing.

---
+++ src/Infrastructure/Stize.Infrastructure.Azure.Tests/KeyVault/KeyTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Pulumi.AzureNative.KeyVault;
+using Pulumi.Testing;
+using Stize.Infrastructure.Tests.Azure.KeyVault.Stacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Stize.Infrastructure.Tests.Azure.KeyVault
+{
+    public class KeyTests
+    {
+        [Fact]
+        public async Task CreateBasicKey()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            key.Should().NotBeNull("Key not found");
+        }
+
+        [Fact]
+        public async Task KeyTypeIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().ToArray();
+            (await key[0].Kty.GetValueAsync()).Should().Be("RSA");
+            (await key[1].Kty.GetValueAsync()).Should().Be("EC");
+        }
+
+        [Fact]
+        public async Task VaultNameIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            (await key.Kty.GetValueAsync()).Should().Be("kv1");
+        }
+
+        [Fact]
+        public async Task RSAKeySizeIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            (await key.KeySize.GetValueAsync()).Should().Be(2048);
+        }
+
+        [Fact]
+        public async Task KeyOperationsIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            (await key.KeyOps.GetValueAsync()).Should().ContainInOrder("encrypt", "decrypt");
+        }
+
+        [Fact]
+        public async Task ECNameIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().LastOrDefault();
+            (await key.CurveName.GetValueAsync()).Should().Be("P_256");
+        }
+
+        [Fact]
+        public async Task IsEnabledIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            (await key.Attributes.GetValueAsync())?.Enabled.Should().Be(true);
+        }
+
+        [Fact]
+        public async Task ActivationDateIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            (await key.Attributes.GetValueAsync())?.NotBefore.Should().Be(1618009200);
+        }
+
+        [Fact]
+        public async Task ExpiryDateIsCorrect()
+        {
+            var resources = await Pulumi.Deployment.TestAsync<KeyBasicStack>(new KeyBasicMock(), new TestOptions { IsPreview = false });
+            var key = resources.OfType<Key>().FirstOrDefault();
+            (await key.Attributes.GetValueAsync())?.Expires.Should().Be(1618095600);
+        }
+    }
+}
---
+++ src/Infrastructure/Stize.Infrastructure.Azure.Tests/KeyVault/Stacks/KeyBasicStack.cs
@@ -0,0 +1,57 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.KeyVault;
+using Stize.Infrastructure.Azure;
+using Stize.Infrastructure.Azure.KeyVault;
+using Stize.Infrastructure.Azure.Networking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stize.Infrastructure.Tests.Azure.KeyVault.Stacks
+{
+    public class KeyBasicStack : Stack
+    {
+        public KeyBasicStack()
+        {
+            var rg = new ResourceGroupBuilder("rg1")
+                .Name("rg1")
+                .Location("westeurope")
+                .Build();
+            var ap1 = new AccessPolicyBuilder()
+                .TenantId("00000000-0000-0000-0000-000000000000")
+                .ObjectId("00000000-0000-0000-0000-000000000000")
+                .CertificatePermissions("all")
+                .KeyPermissions("all")
+                .SecretPermissions("all")
+                .StoragePermissions("all")
+                .Build();
+
+            var kv1 = new VaultBuilder("kv1")
+                .Location("westeurope")
+                .ResourceGroup(rg.Name)
+                .Name("kv1")
+                .TenantId("00000000-0000-0000-0000-000000000000")
+                .AddAccessPolicy(ap1)
+                .Build();
+
+            var key = new KeyBuilder("key1")
+                .Name("key1")
+                .VaultName(kv1.Name)
+                .KeyType(JsonWebKeyType.RSA)
+                .KeySizeInBits(2048)
+                .KeyOps(JsonWebKeyOperation.Encrypt, JsonWebKeyOperation.Decrypt)
+                .ActivationDate("2021-04-10")
+                .ExpiryDate("2021-04-11")
+                .IsEnabled(true)
+                .Build();
+
+            var key2 = new KeyBuilder("key2")
+                .Name("key2")
+                .VaultName(kv1.Name)
+                .KeyType(JsonWebKeyType.EC)
+                .CurveName(JsonWebKeyCurveName.P_256)
+                .IsEnabled(true)
+                .Build();
+        }
+    }
+}
